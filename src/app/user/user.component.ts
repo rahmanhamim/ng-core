ca43ac5f706da28fa9f1e0276454bc4c3f5e3982
rahmanhamim <rahmanhamiminfo@gmail.com>
2025-04-28 21:11:50 +0600

    initialized

---
+++ src/app/user/user.component.ts
@@ -0,0 +1,24 @@
+import { Component, computed, Input, input } from '@angular/core';
+
+@Component({
+  selector: 'app-user',
+  standalone: true,
+  imports: [],
+  templateUrl: './user.component.html',
+  styleUrl: './user.component.css',
+})
+export class UserComponent {
+  // @Input({ required: true }) avatar!: string;
+  // @Input({ required: true }) name!: string;
+
+  avatar = input.required<string>();
+  name = input.required<string>();
+
+  imagePath = computed(() => `assets/users/${this.avatar()}`);
+
+  // get imagePath() {
+  //   return `assets/users/${this.avatar}`;
+  // }
+
+  onSelectUser() {}
+}
