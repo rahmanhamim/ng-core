3c907117c05759be254bbfb471362c9049fdabf1
rahmanhamim <rahmanhamiminfo@gmail.com>
2025-05-02 23:52:45 +0600

    dependency injection

--- src/app/tasks/new-task/new-task.component.ts
+++ src/app/tasks/new-task/new-task.component.ts
@@ -1,12 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
+import { FormsModule, NgModel } from '@angular/forms';
+import { NewTask } from '../task/task.model';
 
 @Component({
   selector: 'app-new-task',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './new-task.component.html',
-  styleUrl: './new-task.component.css'
+  styleUrl: './new-task.component.css',
 })
 export class NewTaskComponent {
+  @Output() cancel = new EventEmitter<void>();
+  @Output() submit = new EventEmitter<NewTask>();
 
+  enteredTitle = '';
+  enteredSummary = '';
+  enteredDueDate = '';
+
+  onCancel() {
+    this.cancel.emit();
+  }
+
+  onSubmit() {
+    this.submit.emit({
+      title: this.enteredTitle,
+      summary: this.enteredSummary,
+      dueDate: this.enteredDueDate,
+    });
+  }
 }
