bd74ad0eb75be01671b36d856ebba59b154058c5
rahmanhamim <rahmanhamiminfo@gmail.com>
2025-05-02 17:08:16 +0600

    working on add task

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,10 +1,5 @@
 import { Component, EventEmitter, Input, Output } from '@angular/core';
-
-interface User {
-  id: string;
-  name: string;
-  avatar: string;
-}
+import { User } from './user.model';
 
 @Component({
   selector: 'app-user',
@@ -15,6 +10,7 @@ interface User {
 })
 export class UserComponent {
   @Input({ required: true }) user!: User;
+  @Input({ required: true }) selected = false;
   @Output() select = new EventEmitter<string>();
 
   // avatar = input.required<string>();
